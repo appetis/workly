4741a7e76eec86fca8a92829f866ccf7848e83ac
hyewon_dev <hyewon.dev@gmail.com>
2021-05-14 11:48:34 -0400

    feat(structure): api flow structure

--- packages/api/app.js
+++ packages/api/app.js
@@ -12,6 +12,9 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+// Reuquire our routes into the application
+require('./server/routes')(app);
+
 // Setup a default catch-all route then sends back a welcome
 app.get('*', (req, res) => res.status(200).send({
     message: 'Welcome to the beginning of nothingness!'
---
+++ packages/api/server/controllers/users.js
@@ -0,0 +1,19 @@
+const User = require('../models').User;
+
+module.exports = {
+    create(req, res) {
+      return User
+          .create({
+              username: req.body.username,
+              password: req.body.password
+          })
+          .then(user => res.status(201).send(user))
+          .catch(error => res.status(400).send(error))
+    },
+    list(req, res) {
+        return User
+            .findAll()
+            .then(users => res.status(200).send(users))
+            .catch(error => res.status(400).send(error))
+    }
+}
