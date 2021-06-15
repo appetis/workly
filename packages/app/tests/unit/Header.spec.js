c6c22f6b8bb9aa36fecf74a231a812f3995639f0
Hyewon Kim <hyewon@Hyewons-MacBook-Pro.local>
2021-06-14 22:34:46 -0400

    feat: add modal profile component

--- packages/app/tests/unit/Header.spec.js
+++ packages/app/tests/unit/Header.spec.js
@@ -1,20 +1,20 @@
-import Header from '../../src/components/Header';
-import { mount } from '@vue/test-utils';
+import Header from '../../src/components/Header'
+import { mount } from '@vue/test-utils'
 import VueFeather from 'vue-feather'
 
 const wrapper = mount(Header, {
-    global: {
-        components: { VueFeather }
-    }
+  global: {
+    components: { VueFeather },
+  },
 })
 describe('Header', () => {
-    test('If search is shown', () => {
-        expect(wrapper.get('#header-icon-search').isVisible()).toBe(true)
-    }),
+  test('If search is shown', () => {
+    expect(wrapper.get('#header-icon-search').isVisible()).toBe(true)
+  }),
     test('If message menu is shown', () => {
-        expect(wrapper.get('#header-icon-message').isVisible()).toBe(true)
+      expect(wrapper.get('#header-icon-message').isVisible()).toBe(true)
     }),
     test('If bell menu is shown', () => {
-        expect(wrapper.get('#header-icon-bell').isVisible()).toBe(true)
+      expect(wrapper.get('#header-icon-bell').isVisible()).toBe(true)
     })
 })
